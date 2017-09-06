2e02928a9c2a4246ffbcb0e9514e3335f6e748d2
Dmlaz <31551206+Dmlaz@users.noreply.github.com>
2017-09-06 19:02:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-// 1. Создаём новый объект XMLHttpRequest
-var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://users.quickblox.com/users.json', false);
 
-// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-xhr.open('GET', 'https://users.quickblox.com/users.json', false);
-
-// 3. Отсылаем запрос
-xhr.send();
-
-// 4. Если код ответа сервера не 200, то это ошибка
-if (xhr.status != 200) {
-  // обработать ошибку
-  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-} else {
-  // вывести результат
-  alert( xhr.responseText ); // responseText -- текст ответа.
-}
-alert("check");
+$.ajax({
+ type: "POST",
+ url:"http://vneformate.ru/android.php",
+ crossDomain: true,
+ cache: false,
+ success: function(data){
+   if(data=="ok")
+   {
+   alert("OK");
+   alert(data);
+   }
+   else if(data=="error")
+   {
+   alert("error");
+   }
+  }
+});
